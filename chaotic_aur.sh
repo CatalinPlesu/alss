8329b1eddbfaf9fc5e3ab47575ed9d033cc9bfaa
Cătălin Pleșu <catalinhimself@gmail.com>
2021-08-24 21:28:32 +0300

    keyring

--- chaotic_aur.sh
+++ chaotic_aur.sh
@@ -3,4 +3,4 @@ pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo "[chaotic-aur]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-pacman -Syu 
+pacman -Syu chaotic-keyring
