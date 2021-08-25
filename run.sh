fcacf1c68e6376eb5ca8d342f6fcd9484aecd975
Cătălin Pleșu <catalinhimself@gmail.com>
2021-08-25 11:23:13 +0300

    reordered

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-source post_chroot.sh
-source dotfiles.sh
+source ./chaotic_aur.sh
+source ./comunity_install.sh
+source ./chaotic_install.sh
 
-source chaotic_aur.sh
-source comunity_install.sh
-source chaotic_install.sh
-
-
+source ./post_chroot.sh
+source ./dotfiles.sh
 
