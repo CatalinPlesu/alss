56afec0a0b733df0ecad916041a641b3bed5d90d
Cătălin Pleșu <catalinhimself@gmail.com>
2021-08-26 19:42:01 +0300

    pacman config

--- config.sh
+++ config.sh
@@ -3,6 +3,13 @@ source ./install.conf
 git clone https://github.com/catalinhimself/dotfiles /home/$username/.config
 ln -s /home/$username/shell/env /home/$username/.zshenv
 chown -R /home/$username $username
- 
+
+sed -i '/Misc options/a\Color' /etc/pacman.conf 
+sed -i '/Misc options/a\ILoveCandy' /etc/pacman.conf 
+sed -i '/Misc options/a\ParallelDownloads = 8' /etc/pacman.conf 
+
+
+echo "permit persist :wheel" >> /etc/doas.conf
+
 wget https://raw.github.com/seanyeh/vibreoffice/master/dist/vibreoffice-0.1.4.oxt
 unopkg add --shared vibreoffice-0.1.4.oxt 
--- doas.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-alias sudo='sudo'
-sudo sh -c 'echo "permit persist :wheel" >> /etc/doas.conf'
-
-
