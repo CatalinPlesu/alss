48c340d1c0ab0952c765e3162faf53ca4e6e2673
Cătălin Pleșu <catalinhimself@gmail.com>
2021-08-28 09:42:26 +0300

    username changed

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source ./install.conf
-git clone https://github.com/catalinhimself/dotfiles /home/$username/.config
+git clone https://github.com/catalinplesu/dotfiles /home/$username/.config
 ln -s /home/$username/shell/env /home/$username/.zshenv
 chown -R /home/$username $username
 
@@ -8,8 +8,11 @@ sed -i '/Misc options/a\Color' /etc/pacman.conf
 sed -i '/Misc options/a\ILoveCandy' /etc/pacman.conf 
 sed -i '/Misc options/a\ParallelDownloads = 8' /etc/pacman.conf 
 
+ln -sfT dash /bin/sh
 
 echo "permit persist :wheel" >> /etc/doas.conf
 
 wget https://raw.github.com/seanyeh/vibreoffice/master/dist/vibreoffice-0.1.4.oxt
 unopkg add --shared vibreoffice-0.1.4.oxt 
+
+
